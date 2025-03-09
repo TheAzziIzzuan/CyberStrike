73474730e75844a4933184c9435e906914e8e5de
zhanyong512 <149478669+zhanyong512@users.noreply.github.com>
2025-03-09 18:26:34 +0800

    Delete main website directory

--- main website/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-setcookie("id", "", time() - 3600, "/");
-setcookie("username", "", time() - 3600, "/");
-setcookie("role", "", time() - 3600, "/");
-
-header("Location: login.html");
-exit();
-?>
